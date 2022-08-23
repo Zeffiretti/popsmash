1ff23540e0fa97d1a360ecd3097b08acb4c02898
Zeffiretti <hiesh@mail.com>
2022-08-23 15:14:56 +0800

    feat: take user input as the circle generation params.
    Signed-off-by: Zeffiretti <hiesh@mail.com>

--- main.cc
+++ main.cc
@@ -1,24 +1,62 @@
-#include "gridmap/circle.h"
 #include "gridmap/gridmap.h"
-#include "gridmap/line.h"
-#include "math/geometry.h"
 #include "mihoyo_macros.h"
 
 #include <random>
 
 const int kWindowWidth = 1000;
 const int kWindowHeight = 1000;
-// #define WINDOW_HEIGHT 800
+
+// argv:
+// 1: number of random circles
+// 2: circle distribution type [uniform, normal, exponential]
+// if 2 is uniform, 3 is the lower bound, 4 is the upper bound. Since the map is
+//    1200x1200, the lower bound should be at least 100, and the upper bound
+//    should be at most 1100.
+// if 2 is normal, 3 is the mean, 4 is the standard deviation. The recommanded mean
+//    is 600.
+// if 2 is exponential, 3 is the lambda
 int main(int argc, char** argv) {
   GridMap gridmap(kWindowWidth, kWindowHeight, 5);
-  // generate 100 random circles with radius 5, x: [0, WINDOW_WIDTH], y: [0, WINDOW_HEIGHT]
+  // get the specific random number distribution
+  int num_circle = 3000;
+  std::string dist_type = "uniform";
+  int dist_arg1 = 100;
+  int dist_arg2 = 1100;
+  if (argc > 1) {
+    num_circle = atoi(argv[1]);
+    dist_type = argv[2];
+    if (dist_type == "uniform") {
+      dist_arg1 = atoi(argv[3]);
+      dist_arg2 = atoi(argv[4]);
+    } else if (dist_type == "normal") {
+      dist_arg1 = atoi(argv[3]);
+      dist_arg2 = atoi(argv[4]);
+    } else if (dist_type == "exponential") {
+      dist_arg1 = atoi(argv[3]);
+    }
+  }
+
   std::random_device rd;
   std::mt19937 gen(rd());
-  std::uniform_int_distribution<> dis(100, 1100);
-  const int num_circle = 3000;
+  std::uniform_int_distribution<> udis(dist_arg1, dist_arg2);
+  std::normal_distribution<> ndis(dist_arg1, dist_arg2);
+  std::exponential_distribution<> edis(dist_arg1);
+
   for (int i = 0; i < num_circle; i++) {
-    int x = dis(gen);
-    int y = dis(gen);
+    int x = 100, y = 100;
+    if (dist_type == "uniform") {
+      x = udis(gen);
+      y = udis(gen);
+    } else if (dist_type == "normal") {
+      x = ndis(gen);
+      y = ndis(gen);
+    } else if (dist_type == "exponential") {
+      x = edis(gen);
+      y = edis(gen);
+    }
+    if (x < 100 || x > 1100 || y < 100 || y > 1100) {  // the circle will not be displayed
+      continue;
+    }
     gridmap.addCircleToMap(x, y, 5);
   }
   gridmap.update();
